833d1a5d13c349c07910cf9dcbd171a50f02379c
Tongyu Chen <chentongyu13@gmail.com>
2018-01-27 16:12:39 -0800

    Solved conflict from last merge

--- version.h
+++ version.h
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-#ifndef _VERSION_H_
-#define _VERSION_H_
-
-#define RSSDK_ENABLED
-#define OPENARK_CAMERA_TYPE "sr300"
-//#define PMDSDK_ENABLED
-//#define OPENARK_CAMERA_TYPE "pmd" 
-
-#endif // _VERSION_H_ 
-=======
 #pragma once
 
 #define RSSDK_ENABLED
@@ -29,4 +18,3 @@ namespace ark {
     // OpenARK version number
     static const char * VERSION = "0.9.0";
 }
->>>>>>> 7f1afe67f2c3263c6a4953394a775219fcac33ed
