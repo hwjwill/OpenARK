d7e4c13b28e488ae1b85dad920e4d6bf6f30d20a
Tongyu Chen <chentongyu13@gmail.com>
2018-01-27 16:05:03 -0800

    Added new classes for mesh generation, not implemented yet.

---
+++ FrameObject.h
@@ -0,0 +1,2 @@
+#pragma once
+#include "Object3D.h"
---
+++ version.h
@@ -0,0 +1,9 @@
+#ifndef _VERSION_H_
+#define _VERSION_H_
+
+#define RSSDK_ENABLED
+#define OPENARK_CAMERA_TYPE "sr300"
+//#define PMDSDK_ENABLED
+//#define OPENARK_CAMERA_TYPE "pmd" 
+
+#endif // _VERSION_H_ 
