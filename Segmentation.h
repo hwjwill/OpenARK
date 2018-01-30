371536f333d740dd1f984d82567a6a5dbfbca589
rnlee0054 <rnlee0054@gmail.com>
2018-01-30 14:22:08 -0800

    Add Segmentation to ark namespace, some comments

--- Segmentation.h
+++ Segmentation.h
@@ -12,61 +12,66 @@
 #include <pcl/segmentation/extract_clusters.h>
 #include <pcl/segmentation/sac_segmentation.h>
 
-class Segmentation
-{
-private:
-    std::vector<pcl::PointCloud<pcl::PointXYZRGBA>::Ptr> mvecpCluster;
-	pcl::PointCloud<pcl::PointXYZRGBA>::Ptr plane;
-
-    /**
-	* Removes the plane from the objects from a point cloud.
-	* @param [in] cloud
-	* @param [out] cloud_plane the cloud representing plane.
-	* @param [out] cloud_objs the cloud representing objects (the rest of points).
-	*/
-    void removePlane(
-            pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud,
-            pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud_plane,
-            pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud_objs
-	);
-
-public:
+namespace ark {
 	/**
-	* Reads a .PCD file.
-	* @param [in] filepath local filepath and filename of .PCD file.
-	* @param [out] cloud the cloud that was stored in the .PCD file
+	* Utility class that allows segmentation (clustering) of an unordered point cloud.
 	*/
-	void readPcd(std::string filepath, pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud);
+	class Segmentation
+	{
+	private:
+		std::vector<pcl::PointCloud<pcl::PointXYZRGBA>::Ptr> mvecpCluster;
+		pcl::PointCloud<pcl::PointXYZRGBA>::Ptr plane;
 
-	/**
-	* Computes the clusters of an input point cloud.
-	* @param [in] cloud
-	* @return vector of clusters, where each is a point cloud pointer
-	*/
-    std::vector<pcl::PointCloud<pcl::PointXYZRGBA>::Ptr> computeClusters(pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud);
+		/**
+		* Removes the plane from the objects from a point cloud.
+		* @param [in] cloud
+		* @param [out] cloud_plane the cloud representing plane.
+		* @param [out] cloud_objs the cloud representing objects (the rest of points).
+		*/
+		void removePlane(
+			pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud,
+			pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud_plane,
+			pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud_objs
+		);
 
-	/**
-	* Performs segmentation on a point cloud of a scene.
-	* Call `getCluster` to retrieve the clusters.
-	* @param [in] cloud scene point cloud
-	*/
-	void segment(pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud);
+	public:
+		/**
+		* Reads a .PCD file.
+		* @param [in] filepath local filepath and filename of .PCD file.
+		* @param [out] cloud the cloud that was stored in the .PCD file
+		*/
+		void readPcd(std::string filepath, pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud);
 
-	/**
-	* Retrieves the clusters without the plane. To be called after `segment`.
-	* @return vector of clusters, where each is a PointCloud
-	*/
-    std::vector<pcl::PointCloud<pcl::PointXYZRGBA>::Ptr> getCluster()
-    {
-        return mvecpCluster;
+		/**
+		* Computes the clusters of an input point cloud.
+		* @param [in] cloud
+		* @return vector of clusters, where each is a point cloud pointer
+		*/
+		std::vector<pcl::PointCloud<pcl::PointXYZRGBA>::Ptr> computeClusters(pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud);
+
+		/**
+		* Performs segmentation on a point cloud of a scene.
+		* Call `getCluster` to retrieve the clusters.
+		* @param [in] cloud scene point cloud
+		*/
+		void segment(pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud);
+
+		/**
+		* Retrieves the clusters without the plane. To be called after `segment`.
+		* @return vector of clusters, where each is a PointCloud
+		*/
+		std::vector<pcl::PointCloud<pcl::PointXYZRGBA>::Ptr> getCluster()
+		{
+			return mvecpCluster;
+		};
+
+		/**
+		* Retrieves the plane cluster. To be called after `segment`.
+		* @return plane cluster (PointCloud)
+		*/
+		pcl::PointCloud<pcl::PointXYZRGBA>::Ptr getPlane()
+		{
+			return plane;
+		};
 	};
-
-	/**
-	* Retrieves the plane cluster. To be called after `segment`.
-	* @return plane cluster (PointCloud)
-	*/
-	pcl::PointCloud<pcl::PointXYZRGBA>::Ptr getPlane()
-    {
-        return plane;
-	};
-};
+}
